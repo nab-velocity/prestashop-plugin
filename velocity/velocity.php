ed62d7dd9a044364e9d89e050c0ffc79e2cc12eb
Ashish Gupta <ashishg2@chetu.com>
2015-09-15 16:34:41 +0530

    hard coded sand box credential removed.

--- velocity/velocity.php
+++ velocity/velocity.php
@@ -11,10 +11,6 @@ if (!defined('_PS_VERSION_'))
 class Velocity extends PaymentModule {
 
 	private $_html = '';
-	private static  $identitytoken;
-	private static  $applicationprofileid;
-	private static  $merchantprofileid;
-	private static  $workflowid;
         private $_postErrors = array();
 
 	/**
@@ -31,10 +27,6 @@ class Velocity extends PaymentModule {
             $this->meta_title           = $this->l('Velocity Merchant Expertise');
             $this->currencies           = true;
             $this->currencies_mode      = 'checkbox';
-            self::$identitytoken        = 'PHNhbWw6QXNzZXJ0aW9uIE1ham9yVmVyc2lvbj0iMSIgTWlub3JWZXJzaW9uPSIxIiBBc3NlcnRpb25JRD0iXzdlMDhiNzdjLTUzZWEtNDEwZC1hNmJiLTAyYjJmMTAzMzEwYyIgSXNzdWVyPSJJcGNBdXRoZW50aWNhdGlvbiIgSXNzdWVJbnN0YW50PSIyMDE0LTEwLTEwVDIwOjM2OjE4LjM3OVoiIHhtbG5zOnNhbWw9InVybjpvYXNpczpuYW1lczp0YzpTQU1MOjEuMDphc3NlcnRpb24iPjxzYW1sOkNvbmRpdGlvbnMgTm90QmVmb3JlPSIyMDE0LTEwLTEwVDIwOjM2OjE4LjM3OVoiIE5vdE9uT3JBZnRlcj0iMjA0NC0xMC0xMFQyMDozNjoxOC4zNzlaIj48L3NhbWw6Q29uZGl0aW9ucz48c2FtbDpBZHZpY2U+PC9zYW1sOkFkdmljZT48c2FtbDpBdHRyaWJ1dGVTdGF0ZW1lbnQ+PHNhbWw6U3ViamVjdD48c2FtbDpOYW1lSWRlbnRpZmllcj5GRjNCQjZEQzU4MzAwMDAxPC9zYW1sOk5hbWVJZGVudGlmaWVyPjwvc2FtbDpTdWJqZWN0PjxzYW1sOkF0dHJpYnV0ZSBBdHRyaWJ1dGVOYW1lPSJTQUsiIEF0dHJpYnV0ZU5hbWVzcGFjZT0iaHR0cDovL3NjaGVtYXMuaXBjb21tZXJjZS5jb20vSWRlbnRpdHkiPjxzYW1sOkF0dHJpYnV0ZVZhbHVlPkZGM0JCNkRDNTgzMDAwMDE8L3NhbWw6QXR0cmlidXRlVmFsdWU+PC9zYW1sOkF0dHJpYnV0ZT48c2FtbDpBdHRyaWJ1dGUgQXR0cmlidXRlTmFtZT0iU2VyaWFsIiBBdHRyaWJ1dGVOYW1lc3BhY2U9Imh0dHA6Ly9zY2hlbWFzLmlwY29tbWVyY2UuY29tL0lkZW50aXR5Ij48c2FtbDpBdHRyaWJ1dGVWYWx1ZT5iMTVlMTA4MS00ZGY2LTQwMTYtODM3Mi02NzhkYzdmZDQzNTc8L3NhbWw6QXR0cmlidXRlVmFsdWU+PC9zYW1sOkF0dHJpYnV0ZT48c2FtbDpBdHRyaWJ1dGUgQXR0cmlidXRlTmFtZT0ibmFtZSIgQXR0cmlidXRlTmFtZXNwYWNlPSJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcyI+PHNhbWw6QXR0cmlidXRlVmFsdWU+RkYzQkI2REM1ODMwMDAwMTwvc2FtbDpBdHRyaWJ1dGVWYWx1ZT48L3NhbWw6QXR0cmlidXRlPjwvc2FtbDpBdHRyaWJ1dGVTdGF0ZW1lbnQ+PFNpZ25hdHVyZSB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC8wOS94bWxkc2lnIyI+PFNpZ25lZEluZm8+PENhbm9uaWNhbGl6YXRpb25NZXRob2QgQWxnb3JpdGhtPSJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzEwL3htbC1leGMtYzE0biMiPjwvQ2Fub25pY2FsaXphdGlvbk1ldGhvZD48U2lnbmF0dXJlTWV0aG9kIEFsZ29yaXRobT0iaHR0cDovL3d3dy53My5vcmcvMjAwMC8wOS94bWxkc2lnI3JzYS1zaGExIj48L1NpZ25hdHVyZU1ldGhvZD48UmVmZXJlbmNlIFVSST0iI183ZTA4Yjc3Yy01M2VhLTQxMGQtYTZiYi0wMmIyZjEwMzMxMGMiPjxUcmFuc2Zvcm1zPjxUcmFuc2Zvcm0gQWxnb3JpdGhtPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwLzA5L3htbGRzaWcjZW52ZWxvcGVkLXNpZ25hdHVyZSI+PC9UcmFuc2Zvcm0+PFRyYW5zZm9ybSBBbGdvcml0aG09Imh0dHA6Ly93d3cudzMub3JnLzIwMDEvMTAveG1sLWV4Yy1jMTRuIyI+PC9UcmFuc2Zvcm0+PC9UcmFuc2Zvcm1zPjxEaWdlc3RNZXRob2QgQWxnb3JpdGhtPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwLzA5L3htbGRzaWcjc2hhMSI+PC9EaWdlc3RNZXRob2Q+PERpZ2VzdFZhbHVlPnl3NVZxWHlUTUh5NUNjdmRXN01TV2RhMDZMTT08L0RpZ2VzdFZhbHVlPjwvUmVmZXJlbmNlPjwvU2lnbmVkSW5mbz48U2lnbmF0dXJlVmFsdWU+WG9ZcURQaUorYy9IMlRFRjNQMWpQdVBUZ0VDVHp1cFVlRXpESERwMlE2ZW92T2lhN0pkVjI1bzZjTk1vczBTTzRISStSUGRUR3hJUW9xa0paeEtoTzZHcWZ2WHFDa2NNb2JCemxYbW83NUFSWU5jMHdlZ1hiQUVVQVFCcVNmeGwxc3huSlc1ZHZjclpuUytkSThoc2lZZW4vT0VTOUdtZUpsZVd1WUR4U0xmQjZJZnd6dk5LQ0xlS0FXenBkTk9NYmpQTjJyNUJWQUhQZEJ6WmtiSGZwdUlablp1Q2l5OENvaEo1bHU3WGZDbXpHdW96VDVqVE0wU3F6bHlzeUpWWVNSbVFUQW5WMVVGMGovbEx6SU14MVJmdWltWHNXaVk4c2RvQ2IrZXpBcVJnbk5EVSs3NlVYOEZFSEN3Q2c5a0tLSzQwMXdYNXpLd2FPRGJJUFpEYitBPT08L1NpZ25hdHVyZVZhbHVlPjxLZXlJbmZvPjxvOlNlY3VyaXR5VG9rZW5SZWZlcmVuY2UgeG1sbnM6bz0iaHR0cDovL2RvY3Mub2FzaXMtb3Blbi5vcmcvd3NzLzIwMDQvMDEvb2FzaXMtMjAwNDAxLXdzcy13c3NlY3VyaXR5LXNlY2V4dC0xLjAueHNkIj48bzpLZXlJZGVudGlmaWVyIFZhbHVlVHlwZT0iaHR0cDovL2RvY3Mub2FzaXMtb3Blbi5vcmcvd3NzL29hc2lzLXdzcy1zb2FwLW1lc3NhZ2Utc2VjdXJpdHktMS4xI1RodW1icHJpbnRTSEExIj5ZREJlRFNGM0Z4R2dmd3pSLzBwck11OTZoQ2M9PC9vOktleUlkZW50aWZpZXI+PC9vOlNlY3VyaXR5VG9rZW5SZWZlcmVuY2U+PC9LZXlJbmZvPjwvU2lnbmF0dXJlPjwvc2FtbDpBc3NlcnRpb24+';
-            self::$applicationprofileid = '14644';
-            self::$merchantprofileid    = 'PrestaShop Global HC';
-            self::$workflowid           = '2317000001';
 
             $this->bootstrap = true;
             parent::__construct();
@@ -42,8 +34,6 @@ class Velocity extends PaymentModule {
             $this->displayName = $this->l('Velocity');
             $this->description = $this->l('Fastest and Secure transaction by Velocity northamericanbancard.');
             $this->confirmUninstall = $this->l('Are you sure you want to delete your details?');
-            if (!isset(self::$workflowid) || !isset(self::$merchantprofileid) || !isset(self::$applicationprofileid) || !isset(self::$identitytoken))
-                    $this->warning = $this->l('Identity Token,Workflowid, ApplicationProfileId & merchantprofileid must be configured before using this module.');
             if (!count(Currency::checkPaymentCurrencies($this->id)))
                     $this->warning = $this->l('No currency has been set for this module.');
                    
@@ -91,7 +81,7 @@ class Velocity extends PaymentModule {
 
             }
 
-            if ( !parent::install() || !$this->registerHook('payment') || !$this->registerHook('orderConfirmation') || !$this->registerHook('displayAdminOrder') || !$this->registerHook('actionProductCancel') || !$this->_installDb())
+            if ( !parent::install() || !$this->registerHook('payment') || !$this->registerHook('orderConfirmation') || !$this->registerHook('displayAdminOrder') || !$this->registerHook('actionOrderReturn') || !$this->registerHook('actionProductCancel') || !$this->_installDb())
                     return false;
             return true;
 	}
@@ -105,7 +95,7 @@ class Velocity extends PaymentModule {
 	{
             $field = Db::getInstance()->ExecuteS("show tables like '%velocity_transaction'");
 
-            if ($field == NULL) {
+            if ($field == NULL) { // if table not created then create table.
                 $status = Db::getInstance()->Execute('
                 CREATE TABLE `'._DB_PREFIX_.'velocity_transaction` (
                 `id_velocity` int(11) NOT NULL AUTO_INCREMENT,
@@ -116,7 +106,7 @@ class Velocity extends PaymentModule {
                 `response_obj` text NOT NULL,
                 PRIMARY KEY (`id_velocity`))
                 ENGINE='._MYSQL_ENGINE_.' DEFAULT CHARSET=utf8 AUTO_INCREMENT=1');
-            } else {
+            } else { // check if table created and all field matched as per new update ortherwise update.
                 foreach($field[0] as $key => $val) {
                     $tablename = $val;
                 }
@@ -130,6 +120,8 @@ class Velocity extends PaymentModule {
                 }
                 if ($count == 0) {
                     $status = Db::getInstance()->Execute("ALTER TABLE " . $tablename . " add request_obj text");
+                } else {
+                    $status = true;
                 }
             }
             return $status;
@@ -309,22 +301,17 @@ class Velocity extends PaymentModule {
 	 */
 	public function getConfigFieldsValues()	{
 
+            $credential = array(
+                'VELOCITY_IDENTITYTOKEN'        => Tools::getValue('VELOCITY_IDENTITYTOKEN', Configuration::get('VELOCITY_IDENTITYTOKEN')),
+                'VELOCITY_WORKFLOWID'           => Tools::getValue('VELOCITY_WORKFLOWID', Configuration::get('VELOCITY_WORKFLOWID')),
+                'VELOCITY_APPLICATIONPROFILEID' => Tools::getValue('VELOCITY_APPLICATIONPROFILEID', Configuration::get('VELOCITY_APPLICATIONPROFILEID')),
+                'VELOCITY_MERCHANTPROFILEID'    => Tools::getValue('VELOCITY_MERCHANTPROFILEID', Configuration::get('VELOCITY_MERCHANTPROFILEID'))
+            );
+            
             if ( Tools::getValue('VELOCITY_TESTMODE_', Configuration::get('VELOCITY_TESTMODE_')) == 'on') {
-                    $credential = array(
-                            'VELOCITY_IDENTITYTOKEN'        => self::$identitytoken,
-                            'VELOCITY_WORKFLOWID'           => self::$workflowid,
-                            'VELOCITY_APPLICATIONPROFILEID' => self::$applicationprofileid,
-                            'VELOCITY_MERCHANTPROFILEID'    => self::$merchantprofileid,
-                            'ISTESTACCOUNT' => true,
-                    );
+                $credential['ISTESTACCOUNT'] = true;
             } else {
-                    $credential = array(
-                            'VELOCITY_IDENTITYTOKEN'        => Tools::getValue('VELOCITY_IDENTITYTOKEN', Configuration::get('VELOCITY_IDENTITYTOKEN')),
-                            'VELOCITY_WORKFLOWID'           => Tools::getValue('VELOCITY_WORKFLOWID', Configuration::get('VELOCITY_WORKFLOWID')),
-                            'VELOCITY_APPLICATIONPROFILEID' => Tools::getValue('VELOCITY_APPLICATIONPROFILEID', Configuration::get('VELOCITY_APPLICATIONPROFILEID')),
-                            'VELOCITY_MERCHANTPROFILEID'    => Tools::getValue('VELOCITY_MERCHANTPROFILEID', Configuration::get('VELOCITY_MERCHANTPROFILEID')),
-                            'ISTESTACCOUNT' => false,
-                    );
+                $credential['ISTESTACCOUNT'] = false;
             }
             return $credential;
 	}
@@ -332,8 +319,7 @@ class Velocity extends PaymentModule {
 	/* 
 	 * @brief Display the decription and logo of velocity payment at backoffice.
 	 */
-	private function _displayVelocity()	{
-	
+	private function _displayVelocity() {
             return $this->display(__FILE__, 'infos.tpl');
 	}
 	
@@ -418,7 +404,7 @@ class Velocity extends PaymentModule {
                 $this->context->cookie->__set('keys','');
             }
         }
-        
+
         /* 
 	 * @brief hook handle the request for standrd refund and products return.
 	 * @param array $params this is hold the detail of order from order_confirmation controler.
@@ -455,28 +441,20 @@ class Velocity extends PaymentModule {
                     $refund_cast = $refund_cast + (float)$shipping_cost;
                 }
                 
-                if( isset($payment[0]->transaction_id) ) {
+                if ( isset($payment[0]->transaction_id) ) {
                     
                     require_once _PS_MODULE_DIR_ . 'velocity/sdk/Velocity.php';
-
                     /* SDK code embeded */
-                    if( Tools::getValue('VELOCITY_TESTMODE_', Configuration::get('VELOCITY_TESTMODE_')) == 'on') {
-
-                        $identitytoken = Velocity::$identitytoken;
-                        $workflowid = Velocity::$workflowid;
-                        $applicationprofileid = Velocity::$applicationprofileid;
-                        $merchantprofileid = Velocity::$merchantprofileid;
+                    
+                    $identitytoken = Tools::getValue('VELOCITY_IDENTITYTOKEN', Configuration::get('VELOCITY_IDENTITYTOKEN'));
+                    $workflowid = Tools::getValue('VELOCITY_WORKFLOWID', Configuration::get('VELOCITY_WORKFLOWID'));
+                    $applicationprofileid = Tools::getValue('VELOCITY_APPLICATIONPROFILEID', Configuration::get('VELOCITY_APPLICATIONPROFILEID'));
+                    $merchantprofileid = Tools::getValue('VELOCITY_MERCHANTPROFILEID', Configuration::get('VELOCITY_MERCHANTPROFILEID'));
+ 
+                    if( Tools::getValue('VELOCITY_TESTMODE_', Configuration::get('VELOCITY_TESTMODE_')) == 'on')
                         $isTestAccount = true;
-
-                    } else {
-
-                        $identitytoken = Tools::getValue('VELOCITY_IDENTITYTOKEN', Configuration::get('VELOCITY_IDENTITYTOKEN'));
-                        $workflowid = Tools::getValue('VELOCITY_WORKFLOWID', Configuration::get('VELOCITY_WORKFLOWID'));
-                        $applicationprofileid = Tools::getValue('VELOCITY_MERCHANTPROFILEID', Configuration::get('VELOCITY_MERCHANTPROFILEID'));
-                        $merchantprofileid = Tools::getValue('VELOCITY_MERCHANTPROFILEID', Configuration::get('VELOCITY_MERCHANTPROFILEID'));
+                    else
                         $isTestAccount = false;
-
-                    }
 
                     /* 
                      * @brief create object of processor class 
@@ -485,6 +463,7 @@ class Velocity extends PaymentModule {
                         $obj_transaction = new Velocity_Processor( $identitytoken, $applicationprofileid, $merchantprofileid, $workflowid, $isTestAccount );
                     } catch (Exception $e) {	
                         $this->context->cookie->__set('keys',$e->getMessage());
+                        return false;
                     }	
 
                     try {
@@ -502,35 +481,40 @@ class Velocity extends PaymentModule {
                             */
                             if ( gettype($res_returnbyid) == 'array' || isset($res_returnbyid['BankcardTransactionResponsePro']['StatusCode'])) {
 
-                            $returnbidres      = json_encode($res_returnbyid);
-                            $transaction_id    = $res_returnbyid['BankcardTransactionResponsePro']['TransactionId'];
-                            $transaction_state = $res_returnbyid['BankcardTransactionResponsePro']['TransactionState'];
-                            $order_id          = $res_returnbyid['BankcardTransactionResponsePro']['OrderId'];
-                            $status            = $res_returnbyid['BankcardTransactionResponsePro']['Status'];
-                            $status_code       = $res_returnbyid['BankcardTransactionResponsePro']['StatusCode'];
-                            $xml               = Velocity_XmlCreator::returnById_XML($refund_cast, $payment[0]->transaction_id);  // got ReturnById xml object. 
-			    $req_obj           = $xml->saveXML();
-                            $req_obj           = serialize($req_obj);
-                            
-                            $transaction = array(
-                                                    'transaction_id'     => $transaction_id,
-                                                    'transaction_status' => $transaction_state,
-                                                    'order_id'           => $order_id,
-                                                    'request_obj'        => $req_obj,
-                                                    'response_obj'       => $returnbidres
-                                                 );
-                            
-                            if(!Db::getInstance()->autoExecute(_DB_PREFIX_.'velocity_transaction', $transaction, 'INSERT'))
-                                $this->context->cookie->__set('keys', 'your transaction is successfull but not save in databsae, please contact technical team.');
-                                return false;
+                                $returnbidres      = json_encode($res_returnbyid);
+                                $transaction_id    = $res_returnbyid['BankcardTransactionResponsePro']['TransactionId'];
+                                $transaction_state = $res_returnbyid['BankcardTransactionResponsePro']['TransactionState'];
+                                $order_id          = $res_returnbyid['BankcardTransactionResponsePro']['OrderId'];
+                                $status            = $res_returnbyid['BankcardTransactionResponsePro']['Status'];
+                                $status_code       = $res_returnbyid['BankcardTransactionResponsePro']['StatusCode'];
+                                $xml               = Velocity_XmlCreator::returnById_XML($refund_cast, $payment[0]->transaction_id);  // got ReturnById xml object. 
+                                $req_obj           = $xml->saveXML();
+                                $req_obj           = serialize($req_obj);
+
+                                $transaction = array(
+                                                        'transaction_id'     => $transaction_id,
+                                                        'transaction_status' => $transaction_state,
+                                                        'order_id'           => $order_id,
+                                                        'request_obj'        => $req_obj,
+                                                        'response_obj'       => $returnbidres
+                                                     );
+
+                                if(!Db::getInstance()->autoExecute(_DB_PREFIX_.'velocity_transaction', $transaction, 'INSERT')) {
+                                    $this->context->cookie->__set('keys', 'your transaction is successfull but not save in databsae, please contact technical team.');
+                                    return false;
+                                } else {
+                                    return true;
+                                }
                             }
                         }	
                     } catch (Exception $e) {
                         $this->context->cookie->__set('keys',$e->getMessage());
+                        return true;
                     }
                     
                 } else {
                     $this->context->cookie->__set('keys','Please contact with technical team transaction is not set.');
+                    return false;
                 }
   
             }
